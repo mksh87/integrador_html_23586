d099345546d5df82729c0aa5b1a4523532f626f1
mksh87 <keshishian.marcos@gmail.com>
2023-11-07 16:23:16 -0300

    Arreglos formulario

--- js/scripts-compras.js
+++ js/scripts-compras.js
@@ -7,11 +7,11 @@ function calcularTotal() {
     entradaJr,
     general;
 
-  console.log('entra a la funcion');
-  cantEntradas = document.getElementById('inputCantidad').value;
-  categoria = document.getElementById('inputCategoria').value;
+  console.log("entra a la funcion");
+  cantEntradas = document.getElementById("inputCantidad").value;
+  categoria = document.getElementById("inputCategoria").value;
 
-  console.log('cant entradas ' + cantEntradas + 'categoria ' + categoria);
+  console.log("cant entradas " + cantEntradas + "categoria " + categoria);
 
   entradaEstudiante = 200 - (200 * 80) / 100;
   entradaTrainee = 200 - (200 * 50) / 100;
@@ -21,33 +21,36 @@ function calcularTotal() {
   /*verifico si el valor ingresado es mayor a cero y si es un número*/
   if (cantEntradas > 0 && !isNaN(cantEntradas) && cantEntradas % 1 == 0) {
     //verifico el tipo de entrada que es (estudiante, trainee y jr para poder mostrar el monto)
-    if (categoria == 'Estudiante') {
+    if (categoria === "Estudiante") {
       total = entradaEstudiante * cantEntradas;
-      document.getElementById('importeTotal').innerHTML =
-        'Total a Pagar: \u0024' + total;
-    } else if (categoria == 'Trainee') {
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: \u0024" + total;
+    } else if (categoria === "Trainee") {
       total = entradaTrainee * cantEntradas;
-      document.getElementById('importeTotal').innerHTML =
-        'Total a Pagar: \u0024' + total;
-    } else if (categoria == 'Junior') {
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: \u0024" + total;
+    } else if (categoria === "Junior") {
       total = entradaJr * cantEntradas;
-      document.getElementById('importeTotal').innerHTML =
-        'Total a Pagar: \u0024' + total;
-    } else if (categoria == 'General') {
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: \u0024" + total;
+    } else if (categoria === "General") {
       total = general * cantEntradas;
-      document.getElementById('importeTotal').innerHTML =
-        'Total a Pagar: \u0024' + total;
+      document.getElementById("importeTotal").innerHTML =
+        "Total a Pagar: \u0024" + total;
+    } else {
+      window.alert("Debe elegir una categoría");
     }
   } else {
-    window.alert('Debe ingresar la cantidad de entradas en números enteros');
+    window.alert("Debe ingresar la cantidad de entradas en números enteros");
   }
 }
 
 function borrar() {
-  document.getElementById('inputNombre').value = ' ';
-  document.getElementById('inputApellido').value = ' ';
-  document.getElementById('inputEmail').value = ' ';
-  document.getElementById('inputCantidad').value = ' ';
-  document.getElementById('inputCategoria').value = 'Estudiante';
-  document.getElementById('importeTotal').innerHTML = 'Total a Pagar: \u0024';
+  document.getElementById("inputNombre").value = "";
+  document.getElementById("inputApellido").value = "";
+  document.getElementById("inputEmail").value = "";
+  document.getElementById("inputCantidad").value = "";
+  document.getElementById("inputCategoria").value = "";
+  document.getElementById("importeTotal").innerHTML = "Total a Pagar: \u0024";
+  $("form :input").removeClass("is-valid is-invalid");
 }
